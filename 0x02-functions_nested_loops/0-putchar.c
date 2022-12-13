3281023a0d862bf87875dd592c374df6a57eb517
yemioke <adeyemialonge@gmail.com>
2022-12-13 09:38:36 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints \"_putchar\*
+ *
+ * Description - prints \*_putchar\* always
+ *
+ * Return - Always 0 (Success)
+ */
+
+int main()
+{
+	printf("_putchar\n");
+	return (0);
+}
