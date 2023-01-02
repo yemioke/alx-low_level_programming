26a0c37a217ff39e7ab0e8560aa07d3040f79772
yemioke <adeyemialonge@gmail.com>
2023-01-02 08:07:19 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -23,10 +23,10 @@ char *_memset(char *s, char b, unsigned int n)
    * value that will always be
    * non-negative (zero or positive)
    */
-  unsigned int i;
+   unsigned int i;
 
   for (i = 0; i < n; i++)
-    s[i] = b;
+          s[i] = b;
 
   return (s);
 }
