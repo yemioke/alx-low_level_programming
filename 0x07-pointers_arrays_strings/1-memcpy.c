6e43680c253d6a0cbf307cf190b1d57cb8b4973e
yemioke <adeyemialonge@gmail.com>
2023-01-02 08:44:43 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 /**
-* _memcpy - function that copies memory area
-*
-* @dest: buffer where we will copy to
-* @src: what we are to copy
-* @n: n bytes of @src
-*
-* Return: Always 0 (Success)
+ * _memcpy - function that copies memory area
+ *
+ * @dest: buffer where we will copy to
+ * @src: what we are to copy
+ * @n: n bytes of @src
+ *
+ * Return: Always 0 (Success)
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -13,6 +13,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	        dest[i] = src[i];
+		dest[i] = src[i];
 	return (dest);
 }
