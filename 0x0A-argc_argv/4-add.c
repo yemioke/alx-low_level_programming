a435f25559e5de123281dcd65eaaa19fbf13590e
yemioke <adeyemialonge@gmail.com>
2023-01-06 11:16:21 -0800

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 {
 	int i, j, add = 0;
 
-	for (i = 1; i < argc; i++);
+	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++);
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 		
-		if (!isdigit(argv[i][j]));
+		if (!isdigit(argv[i][j]))
 															{
 															printf("Error\n");
 															return (1);
